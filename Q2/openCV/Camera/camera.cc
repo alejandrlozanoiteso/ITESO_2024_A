cbe5d36bf513be31ad4bf32a51faf55478439900
Alejandro Lozano <alejandro.llozano@gmail.com>
2024-03-06 15:49:00 -0800

    2024_A: OpenCV Camera Base Project
    Signed-off-by: Alejandro Lozano <alejandro.llozano@gmail.com>

---
+++ Q2/openCV/Camera/camera.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#define WIDHT   640
+#define HEIGHT  480
+
+using namespace cv;
+
+
+int main(int argc, char * argv[]) {
+
+    
+    while ( (char)waitKey(1) != 'q') {
+
+
+    }
+    
+    return 0;
+}
